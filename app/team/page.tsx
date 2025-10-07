9567f9bfc924bc1e8c7d0e750a1fc7f2c2f12189
Hassan <hassanking1241@gmail.com>
2025-10-07 18:02:01 +0500

    up

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 import React from 'react'
 import NavbarTitle from '../about-us/components/NavbarTitle'
@@ -5,12 +6,9 @@ import TeamSection from '@/components/team-section'
 import TeamImg from "@/public/png/Lead software pages Images/Our Team.png"
 import { useEffect } from 'react'
 const page = () => {
-  // Automatically scroll to top when page loads
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
-      // Immediate scroll to top
       window.scrollTo(0, 0)
-  
       setTimeout(() => {
         window.scrollTo({ top: 0, behavior: 'smooth' })
       }, 50)
@@ -19,7 +17,6 @@ const page = () => {
     <div className='w-full overflow-hidden select-none'>
         <NavbarTitle title='Our Team' src={TeamImg}
          />
-         {/* team section */}
          <TeamSection/>
     </div>
   )
