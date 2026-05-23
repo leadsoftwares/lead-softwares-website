96e09ce56e2f92c68d560a5d0e571af6b76fa4f6
Khuram <muhamadkhuram1999@gmail.com>
2026-05-23 23:58:26 +0500

    build in production

--- components/google-tag.tsx
+++ components/google-tag.tsx
@@ -1,60 +1,25 @@
-'use client'
-
-import { useEffect } from 'react'
-import { analyticsConfig } from '../lib/analytics-config'
-
-declare global {
-	interface Window {
-		dataLayer?: unknown[]
-		gtag?: (...args: unknown[]) => void
-	}
-}
+import Script from 'next/script'
 
 export default function GoogleTag() {
-	useEffect(() => {
-		if (!analyticsConfig.shouldTrack()) {
-			return
-		}
-
-		const tagId = analyticsConfig.googleTag.tagId
-
-		if (!tagId) {
-			console.log('Google Tag: Skipped - NEXT_PUBLIC_GOOGLE_TAG_ID is not set')
-			return
-		}
-
-		if (
-			document.querySelector(
-				`script[src*="googletagmanager.com/gtag/js?id=${tagId}"]`,
-			)
-		) {
-			return
-		}
-
-		window.dataLayer = window.dataLayer || []
-		window.gtag = (...args: unknown[]) => {
-			window.dataLayer?.push(args)
-		}
-
-		const script = document.createElement('script')
-		script.async = true
-		script.src = `https://www.googletagmanager.com/gtag/js?id=${tagId}`
-		document.head.appendChild(script)
-
-		window.gtag('js', new Date())
-		window.gtag('config', tagId)
-
-		console.log(`Google Tag: Initialized with tag ID ${tagId}`)
-
-		return () => {
-			const tagScript = document.querySelector(
-				`script[src*="googletagmanager.com/gtag/js?id=${tagId}"]`,
-			)
-			if (tagScript) {
-				tagScript.remove()
-			}
-		}
-	}, [])
-
-	return null
+	return (
+		<>
+			<Script
+				async
+				src='https://www.googletagmanager.com/gtag/js?id=AW-10816304544'
+				strategy='afterInteractive'
+			/>
+			<Script
+				id='google-ads-gtag'
+				strategy='afterInteractive'
+				dangerouslySetInnerHTML={{
+					__html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'AW-10816304544');
+`,
+				}}
+			/>
+		</>
+	)
 }
